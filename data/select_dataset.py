9629694ae872597468b35269f01e612543123d57
BP113 <168256109+BP113@users.noreply.github.com>
2024-05-24 00:16:50 +0800

    Add files via upload

---
+++ data/select_dataset.py
@@ -0,0 +1,42 @@
+
+
+'''
+# --------------------------------------------
+# select dataset
+# --------------------------------------------
+# Kai Zhang (github: https://github.com/cszn)
+# --------------------------------------------
+'''
+
+
+def define_Dataset(dataset_opt):
+    dataset_type = dataset_opt['dataset_type'].lower()
+    if dataset_type in ['l', 'low-quality', 'input-only']:
+        from data.dataset_l import DatasetL as D
+
+    # -----------------------------------------
+    # denoising
+    # -----------------------------------------
+    elif dataset_type in ['dncnn', 'denoising']:
+        from data.dataset_dncnn import DatasetDnCNN as D
+
+
+    elif dataset_type in ['ufpnet']:
+        from data.dataset_ufpnet import DatasetUFPNet as D
+
+
+    # -----------------------------------------
+    # common
+    # -----------------------------------------
+    elif dataset_type in ['plain']:
+        from data.dataset_plain import DatasetPlain as D
+
+    elif dataset_type in ['plainpatch']:
+        from data.dataset_plainpatch import DatasetPlainPatch as D
+
+    else:
+        raise NotImplementedError('Dataset [{:s}] is not found.'.format(dataset_type))
+
+    dataset = D(dataset_opt)
+    print('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__, dataset_opt['name']))
+    return dataset
